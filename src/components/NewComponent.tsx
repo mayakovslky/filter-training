bf67751bdaac051b3097f4a19802218293aa3a39
mayakovsky <mayakovsky03@gmail.com>
2022-12-03 19:40:54 +0300

    02. method map training and types

---
+++ src/components/NewComponent.tsx
@@ -0,0 +1,47 @@
+import React from "react";
+
+type StudentsType = {
+    id: number
+    name: string
+    age: number
+}
+
+type TopCarsType = {
+    manufacturer: string
+    model: string
+}
+
+type NewComponentType = {
+    // students: Array<StudentsType>
+    students: StudentsType[]
+    topCars: TopCarsType[]
+}
+
+
+
+export const NewComponent = (props: NewComponentType) => {
+
+    return (
+        <>
+            <ul>
+                {props.students.map((o) => {
+                    return (
+                        <li key={o.id}>
+                            <span>{o.name} - </span>
+                            <span>age: {o.age}</span>
+                        </li>
+                    )
+                })}
+                <br/>
+                {props.topCars.map((c, index) => {
+                    return (
+                        <th key={index}>
+                                <tr>{c.manufacturer}</tr>
+                            <tr>{c.model}</tr>
+                        </th>
+                    )
+                })}
+            </ul>
+        </>
+    )
+}
